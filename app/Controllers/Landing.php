e5d93bd73b8ef1ede7988b82cf8f9a88c8ac8d92
Mohammad Jamil Asfihani <51300528+mjamilasfihani@users.noreply.github.com>
2021-07-10 19:33:26 +0700

    Update Landing.php

--- app/Controllers/Landing.php
+++ app/Controllers/Landing.php
@@ -20,23 +20,10 @@ class Landing extends BaseController
 		// Calling the library
 		$template = new \App\Libraries\AssetsLoader();
 
-		// A little Body conguration
-		$template->body([
-			'attributes' => ['id' => 'landing-page'],
-			'preload' => true
-		]);
-
-		// A little Html configuration
-		$template->html([
-			'language' => 'en',
-			'title' => 'Coming Soon'
-		]);
-
-		// A little Meta configuration
-		$template->meta([
-			'author' => 'Website Developer',
-			'description' => 'Thank you for visiting our website, currently this website is coming soon.'
-		]);
+		// A little conguration
+		$template->body(['preload' => true]);
+		$template->html(['title' => 'Coming Soon']);
+		$template->meta(['description' => 'Thank you for visiting our website, currently this website is coming soon.']);
 
 		// Render it
 		return $template->render(view($name, $data, $options));
